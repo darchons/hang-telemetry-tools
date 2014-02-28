b9dd65d546599ec8e973830ea7685aef4ea32d46
Jim Chen <nchen@mozilla.com>
2014-02-28 15:42:52 -0500

    Skip empty BHR stacks

--- mapreduce-bhr.py
+++ mapreduce-bhr.py
@@ -62,6 +62,8 @@ def map(raw_key, raw_dims, raw_value, cx):
         cx.write((name, None),
                  collectData(dims, info, thread['activity']['values']))
         for hang in thread['hangs']:
+            if not hang['stack']):
+                continue
             cx.write((name, tuple(filterStack(hang['stack']))),
                      collectData(dims, info, hang['histogram']['values']))
         cx.write((None, name), collectedUptime)
